3954d3b52e5294dae13e36fca3c166732e8a9dc0
BarryChen <s481838@sinyi.com.tw>
2024-08-20 17:02:17 +0800

    update

---
+++ src/components/MyCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps(['inner-data', 'value'])
+console.log(props)
+
+const emit = defineEmits(['emitTitle'])
+const emitOuterTitle = () => {
+  console.log('內層觸發Emit')
+  emit('emitTitle', '內層要修改外層的資料')
+}
+</script>
+
+<template>
+  <div class="card">
+    <img class="card-img-top" :src="innerData.imgUrl" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{ innerData.title }}</h5>
+      <p class="card-text">
+        {{ innerData.content }}
+      </p>
+      <input type="text" v-model="innerData.title" />
+      <a href="#" class="btn btn-primary" @click="emitOuterTitle">Go somewhere</a><br />
+      <a href="#" @click.prevent="emit('emitTitle', innerData.title)">傳遞事件</a>
+    </div>
+  </div>
+</template>
--- src/views/EasySample.vue
+++ src/views/EasySample.vue
@@ -1,13 +1,37 @@
 <script setup>
+import { ref } from 'vue'
 import EasyHeader from '@/components/EasyHeader.vue'
 import EasyCard from '@/components/EasyCard.vue'
 import EasyFooter from '@/components/EasyFooter.vue'
+import MyCard from '@/components/MyCard.vue'
+
+const data = ref({
+  title: '這是一張卡片',
+  content:
+    'Lorem ipsum dolor sit amet, wisi perfecto invenire sed ei, ea quod inciderint eam. Eu dolor legimus necessitatibus nam, ne quo civibus signiferumque. Ex partem prompta iracundia sea, eum erat minim efficiantur at. Ei vel noster viderer gubergren, mea falli urbanitas id.',
+  imgUrl:
+    'https://images.unsplash.com/photo-1723663758069-f62e8a5db92e?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+})
+
+const value = ref('這是外層單向數據流的文字')
+
+const editTitle = (newTitle) => {
+  console.log('editTitle', newTitle)
+  data.value.title = newTitle
+}
 </script>
 
 <template>
   <!-- 不要拆元件 -->
   <div class="container">
     <EasyHeader />
+
+    <div class="col-4">
+      <!-- props 前內後外 -->
+      <!-- 實戰通常會使用同名 -->
+      <MyCard v-bind:inner-data="data" :value="value" @emit-title="editTitle" />
+    </div>
+
     <div class="row g-4">
       <div class="col-4" v-for="i in 10" :key="i">
         <EasyCard />
